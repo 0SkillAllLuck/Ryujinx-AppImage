a6e9dce4ba2e49d38a5aca7c00f832847d6be72a
Cedric Lewe <0SkillAllLuck@pm.me>
2024-11-15 17:41:11 +0100

    Update script to be more efficient

--- build.sh
+++ build.sh
@@ -7,63 +7,54 @@ REPOSITORY="GreemDev/Ryujinx"
 
 # Setup directories
 buildDir="./build"
+appDir="$buildDir/$APP.AppDir"
+distDir="./dist"
 if [ -d "$buildDir" ]; then
   rm -rf "$buildDir"
 fi
 mkdir -p "$buildDir"
-
-# Download appimagetool
-wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O "$buildDir/appimagetool"
-chmod a+x "$buildDir/appimagetool"
+mkdir -p "$appDir/usr/bin"
+mkdir -p "$distDir"
 
 # Download ryujinx release information
 echo "Downloading Ryujinx Release information"
 releaseURL=$(wget -q https://api.github.com/repos/$REPOSITORY/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi "https:\/\/.*linux_x64\.tar\.gz$" | head -1)
-version="$(echo "$releaseURL" | awk -F"/" '{print $(NF-1)}')"
+releaseVersion="$(echo "$releaseURL" | awk -F"/" '{print $(NF-1)}')"
 
 # Download appimage release information
 echo "Downloading AppImage Release information"
 appImageReleaseURL=$(wget -q https://api.github.com/repos/0SkillAllLuck/Ryujinx-AppImage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi "https:\/\/.*x86_64\.AppImage$" | head -1)
 appImageVersion="$(echo "$appImageReleaseURL" | awk -F"/" '{print $(NF-1)}')"
+
 # Check if the AppImage is the same version as the release
-if [ "$appImageVersion" = "$version" ]; then
+if [ "$appImageVersion" = "$releaseVersion" ]; then
   echo "AppImage already up to date"
   exit 0
 fi
 
-# Download ryujinx release
-echo "Downloading Ryujinx version: $version"
+# Download Ryujinx release
+echo "Downloading Ryujinx version: $releaseVersion"
 wget -q "$releaseURL" -O "$buildDir/ryujinx.tar.gz"
 tar xf "$buildDir/ryujinx.tar.gz" -C "$buildDir"
 
-# Create AppDir and download desktop and icon files
-appDir="$buildDir/$APP.AppDir"
-mkdir -p "$appDir/usr/bin"
-mv "$buildDir/publish/"* "$appDir/usr/bin"
-
-# Create desktop and icon files
+# Downloading AppImage files
 echo "Downloading desktop and icon files"
+wget -q https://raw.githubusercontent.com/$REPOSITORY/master/distribution/linux/appimage/AppRun -O "$appDir/AppRun"
 wget -q https://raw.githubusercontent.com/$REPOSITORY/master/distribution/linux/Ryujinx.desktop -O "$appDir/$APP.desktop"
-wget -q https://raw.githubusercontent.com/$REPOSITORY/master/src/Ryujinx/Ryujinx.ico -O "$appDir/Ryujinx.png"
-ln -s "$appDir/Ryujinx.png" "$appDir/.DirIcon"
+wget -q https://raw.githubusercontent.com/$REPOSITORY/master/src/Ryujinx.UI.Common/Resources/Logo_Ryujinx.png -O "$appDir/Ryujinx.png"
+chmod +x "$appDir/AppRun"
 
-# Create AppRun
-echo "Creating AppRun"
-cat >> "$appDir/AppRun" << 'EOF'
-#!/bin/sh
-CURRENTDIR="$(readlink -f "$(dirname "$0")")"
-exec "$CURRENTDIR"/usr/bin/Ryujinx.sh "$@"
-EOF
-chmod a+x "$appDir/AppRun"
+# Download appimagetool
+echo "Downloading appimagetool"
+wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O "$buildDir/appimagetool"
+chmod a+x "$buildDir/appimagetool"
 
 # Build the AppImage
-distDir="./dist"
-appimageFile="$distDir/$APP-$version-x86_64.AppImage"
-mkdir -p "$distDir"
-echo "Building AppImage: $appimageFile"
-./build/appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
+echo "Building AppImage"
+mv "$buildDir/publish/"* "$appDir/usr/bin"
+./build/appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 21 \
   -u "gh-releases-zsync|0SkillAllLuck|Ryujinx-AppImage|latest|*x86_64.AppImage.zsync" \
-  "$appDir" "$appimageFile"
+  "$appDir" "$distDir/$APP-$releaseVersion-x86_64.AppImage"
 
 # Save version to file
-echo "VERSION=$version" > "$distDir/version"
+echo "VERSION=$releaseVersion" > "$distDir/version"
